4e8bd524877ded6a837a9487f6a80cafef5e922e
arpitspecscale <arpitv.specscale@gmail.com>
2023-11-21 18:30:43 +0530

    review create

--- src/Pages/LandingPage/Home/home.tsx
+++ src/Pages/LandingPage/Home/home.tsx
@@ -3,11 +3,13 @@ import "../Home/home.css"
 import Banner from './banner'
 import About from './about'
 import Footer from './footer'
+import Review from './review'
 const Home = () => {
     return (
         <div>
             <Banner />
             <About />
+            <Review />
             <Footer />
         </div>
     )
