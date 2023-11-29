94464723a3be4fae91ec8e24cf4d343d880cc2e9
Rutubhimani28 <rutubhimani.specscale@gmail.com>
2023-11-29 15:30:43 +0530

    build error solve

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { AuthProvider } from "./context/authcontext";
 import Routing from "./routes";
 import { useNavigate } from "react-router-dom";
 import AddListing from "./Pages/LandingPage/addListing/AddListing";
-import Login from "./Pages/auth/Login/login";
+import Login from "./Pages/auth/Login/Login";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
