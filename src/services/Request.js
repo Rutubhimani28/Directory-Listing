53bef72818afd7752e19934f5cd221d644a17c77
Rutubhimani28 <rutubhimani.specscale@gmail.com>
2023-11-29 17:48:26 +0530

    ragister api changes

--- src/services/Request.js
+++ src/services/Request.js
@@ -2,55 +2,56 @@ import axios from "axios";
 import Api from "./api";
 
 export default class Requests {
-  signUpRequest(values) {
-    return axios({
-      method: "POST",
-      url: `${Api.signupUser}`,
-      values,
-    });
-  }
+    signUpRequest(data) {
+        console.log(data, "OOOOOOOOOOOOOOOOOOOO")
+        return axios({
+            method: "POST",
+            url: `${Api.signupUser}`,
+            data: data,
+        });
+    }
 
-  loginRequest(data) {
-    return axios({
-      method: "POST",
-      url: `${Api.loginUser}`,
-      data,
-    });
-  }
+    loginRequest(data) {
+        return axios({
+            method: "POST",
+            url: `${Api.loginUser}`,
+            data,
+        });
+    }
 
-  forgotPassword(data) {
-    return axios({
-      method: "POST",
-      url: `${Api.forgotPassword}`,
-      data,
-      // headers: { Authorization: `TOKEN ${localStorage.getItem('Token')}` }
-    });
-  }
+    forgotPassword(data) {
+        return axios({
+            method: "POST",
+            url: `${Api.forgotPassword}`,
+            data,
+            // headers: { Authorization: `TOKEN ${localStorage.getItem('Token')}` }
+        });
+    }
 
-  resetPassword(data) {
-    return axios({
-      method: "POST",
-      url: `${Api.resetPassword}`,
-      data,
-      // headers: { Authorization: `TOKEN ${localStorage.getItem('Token')}` },
-      // body : JSON.parse()
-    });
-  }
-  addListingUser(data) {
-    return axios({
-      method: "POST",
-      url: `${Api.addListing}`,
-      data,
-    });
-  }
+    resetPassword(data) {
+        return axios({
+            method: "POST",
+            url: `${Api.resetPassword}`,
+            data,
+            // headers: { Authorization: `TOKEN ${localStorage.getItem('Token')}` },
+            // body : JSON.parse()
+        });
+    }
+    addListingUser(data) {
+        return axios({
+            method: "POST",
+            url: `${Api.addListing}`,
+            data,
+        });
+    }
 
-  profileImage(data) {
-    return axios({
-      method: "POST",
-      url: `${Api.profileImage}`,
-      data,
-      // headers: { Authorization: `TOKEN ${localStorage.getItem('Token')}` },
-      // body : JSON.parse()
-    });
-  }
+    profileImage(data) {
+        return axios({
+            method: "POST",
+            url: `${Api.profileImage}`,
+            data,
+            // headers: { Authorization: `TOKEN ${localStorage.getItem('Token')}` },
+            // body : JSON.parse()
+        });
+    }
 }
